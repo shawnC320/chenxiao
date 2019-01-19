8a4c50299db29119985b11c593f1c946f606c980
shawnC320 <43753897+shawnC320@users.noreply.github.com>
2019-01-19 13:51:02 +0800

    Update array.py

--- chenxiao/array.py
+++ chenxiao/array.py
@@ -1,30 +1,39 @@
 class Stack(object):
 
 
-    # 构建两个空栈 array[0] array[1]
+    # 构建数组
     def __init__(self):
-        self.array = [[], []]
-
+        self.array = []
+        self.array1 = []
+        self.array2 = []
+        
+    # 依据下标奇偶将数组分为两个栈 array[0] array[1]
+    def divArray(self):
+        for i in range(len(self.array), 2):
+            self.array1.append(i)
+        for i in range(1, len(self.array), 2):
+            self.array2.append(i)
+            
     # 左侧删除 弹出
     def l_pop(self):
         # 判断array[0]是否为空
-        if len(self.array[0]) == 0:
-            print('Stack b is empty')
+        if len(self.array1) == 0:
+            print('Stack array1 is empty')
         else:
-            self.array[0].pop(0)
+            self.array1.pop(0)
 
     # 左侧插入 压栈
     def l_push(self, data):
-        self.array[0].insert(0, data)
+        self.array1.insert(0, data)
  
     # 右侧删除 弹出
     def r_pop(self):
         # 判断array[1]是否为空
-        if len(self.array[1]) == 0:
-            print('Stack a is empty')
+        if len(self.array2) == 0:
+            print('Stack array2 is empty')
         else:
-            self.array[1].pop()
+            self.array2.pop()
 
     # 右侧插入 压栈
     def r_push(self, data):
-        self.array[1].append(data)
+        self.array2.append(data)
